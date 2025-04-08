3fbebf02da62139d36eeb5f90f1ff6abdae1de6e
Aditya <adityasahoo652@gmail.com>
2025-04-08 23:17:06 +0530

    feat: set up Express server with CORS, routes, and error handling middleware

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.resolve("src/uploads")));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to socialfi media API");
+});
+
+// Routes
+import authRoutes from "./routes/auth";
+
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  console.log(`CORS is enabled for ${process.env.CORS_ORIGIN}`);
+});
+
+// Error handling middleware
+import { errorMiddleware } from "./middlewares/error";
+app.use(errorMiddleware);
