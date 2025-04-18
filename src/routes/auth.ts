7007826eb475c01256b35e69f812d3d5312db565
Aditya <adityasahoo652@gmail.com>
2025-04-18 20:05:04 +0530

    refact: update multer middleware import for file handling in registration route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { profilePicture } from "../middlewares/multer";
+import { files } from "../middlewares/multer";
 import { login, logout, register } from "../controllers/auth";
 import { authRateLimiter } from "../middlewares/rateLimiter";
 
 const router = express.Router();
 
-router.post("/register", authRateLimiter, profilePicture, register);
+router.post("/register", authRateLimiter, files, register);
 router.post("/login", authRateLimiter, login);
 router.get("/logout", logout);
 
