f2b2b71659fbd033dc7c65d54fdf05f292fe6253
Aditya <adityasahoo652@gmail.com>
2025-04-18 20:20:50 +0530

    feat: add delete profile route and update multer middleware import

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth";
 import {
+  deleteProfile,
   followUser,
   getFollowers,
   getFollowings,
@@ -8,16 +9,17 @@ import {
   unfollowUser,
   updateProfile,
 } from "../controllers/user";
-import { profilePicture } from "../middlewares/multer";
+import { files } from "../middlewares/multer";
 
 const router = express.Router();
 
 router
   .get("/:id", getUserProfile)
-  .put("/:id", isAuthenticated, profilePicture, updateProfile)
+  .put("/:id", isAuthenticated, files, updateProfile)
   .get("/:id/followers", isAuthenticated, getFollowers)
   .get("/:id/following", isAuthenticated, getFollowings)
   .post("/:id/follow", isAuthenticated, followUser)
-  .post("/:id/unfollow", isAuthenticated, unfollowUser);
+  .post("/:id/unfollow", isAuthenticated, unfollowUser)
+  .delete("/:id", isAuthenticated, deleteProfile);
 
 export default router;
